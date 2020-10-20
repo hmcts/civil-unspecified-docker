237f4670df05ba4bfc7582290e0b27b7afb2886e
Sam Walker <samw@kainos.com>
2020-10-20 09:58:39 +0100

    added mo app compose

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -9,3 +9,5 @@ ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_sec
 ${dir}/utils/idam-create-service.sh "unspec" "unspec" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
 
 ${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user"
+
+${dir}/utils/idam-create-service.sh "xui_webapp_mo" "xui_webapp_mo" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback"
