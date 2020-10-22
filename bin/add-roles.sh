6735e37682390344e0e173f087ceb0fbe2e8c99c
Sam Walker <samw@kainos.com>
2020-10-22 16:19:46 +0100

    fix typo in case manage env variable

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -4,12 +4,11 @@ set -eu
 
 dir=$(dirname ${0})
 
-${dir}/utils/idam-add-role.sh "ccd-import"
-
-# User used during the CCD import and ccd-role creation
-${dir}/utils/idam-create-caseworker.sh "ccd.docker.default@hmcts.net" "ccd-import"
-
 jq -r '[(.[] | .roles | split(",")) | .[] ] | unique[]' ${dir}/users.json | while read args; do
   ${dir}/utils/idam-add-role.sh "$args"
   ${dir}/utils/ccd-add-role.sh "$args"
 done
+
+${dir}/utils/idam-add-role.sh "ccd-import"
+# User used during the CCD import and ccd-role creation
+${dir}/utils/idam-create-caseworker.sh "ccd.docker.default@hmcts.net" "ccd-import"
