071208f389166a71b73521730f70bd65514e2f31
Sam Walker <samw@kainos.com>
2020-10-22 14:01:03 +0100

    added case assignment yml

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -10,4 +10,6 @@ ${dir}/utils/idam-create-service.sh "unspec" "unspec" "OOOOOOOOOOOOOOOO" "https:
 
 ${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user"
 
-${dir}/utils/idam-create-service.sh "xui_webapp_mo" "xui_webapp_mo" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback"
+${dir}/utils/idam-create-service.sh "xui_webapp_mo" "xui_webapp_mo" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user manage-roles"
+
+${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid roles profile search-user"
